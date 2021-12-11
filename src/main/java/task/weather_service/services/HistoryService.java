729431ad42b126a20c6ce85d3715a427c742372b
crangoren <crangoren@gmail.com>
2021-12-11 20:13:19 +0300

    add project

--- src/main/java/task/weather_service/services/HistoryService.java
+++ src/main/java/task/weather_service/services/HistoryService.java
@@ -7,6 +7,11 @@ import java.net.URL;
 
 @Service
 public class HistoryService {
+/**
+ * В требованиях к тестовому было указано стучаться именно на яндекс, без сторонних библиотек.
+ * сделал так. Как вариант можно было бы через их weather api на тестовом аккаунте и получить стразу JSON объект,
+ * либо с использованием jsoap, но так же искать url с бэка.
+ * **/
 
     public String getTemp() throws IOException {
         URL url = new URL("https://yandex.ru/?utm_source=google&utm_medium=search&utm_campaign=brand_search_msk%3Aspb&utm_term=yandex&gclid=CjwKCAiAksyNBhAPEiwAlDBeLD6DBZ3K2NEm2GbpAWTh1_lAaJ9dNLgsUtLajAeSMmZSQTYHLbzh5RoChuUQAvD_BwE&campaignid=1679697480&adgroupid=65697694936&creative=438922846234&keyword=yandex");
