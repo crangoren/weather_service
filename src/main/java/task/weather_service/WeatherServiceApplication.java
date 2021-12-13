0933c2d10ee01bfb0a1798c4b340af672f22776f
crangoren <crangoren@gmail.com>
2021-12-13 13:23:35 +0300

    add task

--- src/main/java/task/weather_service/WeatherServiceApplication.java
+++ src/main/java/task/weather_service/WeatherServiceApplication.java
@@ -1,5 +1,17 @@
 package task.weather_service;
 
-	public static void main(String[] args) {
+import org.hibernate.SessionFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import task.weather_service.utils.SessionFactoryUtils;
 
+@SpringBootApplication
+public class WeatherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherServiceApplication.class, args);
+		SessionFactoryUtils sessionFactoryUtils = new SessionFactoryUtils();
+		sessionFactoryUtils.init();
+
+	}
 }
--- src/main/java/task/weather_service/controllers/WeatherController.java
+++ src/main/java/task/weather_service/controllers/WeatherController.java
@@ -1,6 +1,25 @@
 package task.weather_service.controllers;
 
-public class WeatherController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import task.weather_service.services.WeatherService;
+import java.io.IOException;
+import java.util.Date;
 
+@RestController
+public class WeatherController {
+    private final WeatherService weatherService;
+
+    @Autowired
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/weather")
+    public String showWeather() throws IOException {
+        Date curDate = new Date();
+        return weatherService.showWeather();
+    }
 
 }
