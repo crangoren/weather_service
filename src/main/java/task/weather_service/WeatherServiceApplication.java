3bf185da2fbdaba95fa097cd9bf3754f94618d86
crangoren <crangoren@gmail.com>
2021-12-13 15:13:03 +0300

    add task

--- src/main/java/task/weather_service/WeatherServiceApplication.java
+++ src/main/java/task/weather_service/WeatherServiceApplication.java
@@ -3,15 +3,12 @@ package task.weather_service;
 import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import task.weather_service.utils.SessionFactoryUtils;
+//import task.weather_service.utils.SessionFactoryUtils;
 
 @SpringBootApplication
 public class WeatherServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherServiceApplication.class, args);
-		SessionFactoryUtils sessionFactoryUtils = new SessionFactoryUtils();
-		sessionFactoryUtils.init();
-
 	}
 }
